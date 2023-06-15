b71a2c47df70a2a5885b6a17f5d0c753ca05bcac
jecanart <jecanart@espol.edu.ec>
2023-06-14 19:23:46 -0500

    Correccion de errores

--- frontend/public/javascript/calculadora.js
+++ frontend/public/javascript/calculadora.js
@@ -3,15 +3,15 @@ let funcionPredeterminada = () => {
     formulario.addEventListener('submit',(evento) => {
         evento.preventDefault();
         let vHora = parseFloat(document.getElementById('valorPorHora').value)
-        let vInterfaz = parseFloat(document.getElementById('interfazHora').value)
+        let vInterfaz = parseFloat(document.getElementById('interfazHoras').value)
         let vHTML = parseFloat(document.getElementById('horasHTML').value)
-        let vCSS = parseFloat(document.getElementById('horasCSS').value)
-        let vJava = parseFloat(document.getElementById('horasJava').value)
+        let vCSS = parseFloat(document.getElementById('hCSS').value)
+        let vJava = parseFloat(document.getElementById('hJava').value)
         
 
         let valorTotal = (vHora *  vInterfaz ) + (vHora * vHTML) + (vHora * vCSS) + (vHora * vJava);
 
-        document.getElementById('valorTotal').value = valorTotal ;
+        document.getElementById('valorTotal').value= valorTotal ;
         
 
     })
